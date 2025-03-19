4d2b5bac2cdc1c69e925f3a3f6de0e389ba24270
Keshav Saraogi <78319655+KeshavSaraogi@users.noreply.github.com>
2025-03-20 01:17:50 +0530

    user interaction
    -- completing user interaction
    -- debugging application

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ from langchain.agents               import Tool, initialize_agent
 from langchain.callbacks            import StreamlitCallbackHandler
 from langchain_community.utilities  import WikipediaAPIWrapper
 from langchain.agents.agent_types   import AgentType
-
 from dotenv import load_dotenv
+
 load_dotenv()
 
 ## Set up the Streamlit app
@@ -26,7 +26,7 @@ llm = ChatGroq(model = "gemma2-9b-it", groq_api_key = groqApiKey)
 wikipediaWrapper = WikipediaAPIWrapper()
 wikipediaTool = Tool(
     name = "Wikipedia",
-    function = wikipediaWrapper.search,
+    func = wikipediaWrapper.run,
     description = "Search and Solve Wikipedia for information on the topics mentioned"
 )
 
@@ -34,7 +34,7 @@ wikipediaTool = Tool(
 mathChain = LLMMathChain.from_llm(llm = llm)
 calculator = Tool(
     name = "Calculator",
-    function = mathChain.run,
+    func = mathChain.run,
     description = "Tool for answering math problems. Only mathematically expressions are supported."
 )
 
@@ -46,7 +46,7 @@ Answer:
 """
 
 promptTemplate = PromptTemplate(
-    input_variables = ['questions'],
+    input_variables = ['question'],
     template = prompt
 )
 
@@ -54,7 +54,7 @@ promptTemplate = PromptTemplate(
 chain = LLMChain(llm = llm, prompt = promptTemplate)
 
 reasoningTool = Tool(
-    name = 'Reasoing Tool',
+    name = 'Reasoning Tool',
     func = chain.run,
     description = 'Reasoning Tool for answering logic-based and reasoning questions'
 )
@@ -62,8 +62,8 @@ reasoningTool = Tool(
 ## Initialize the agents
 assistantAgent = initialize_agent(
     tools = [wikipediaTool, calculator, reasoningTool],
-    llm = llm, 
-    agent = AgentType.ZERO_SHOT_REACT_DESCRIPTIION,
+    llm = llm,
+    agent = AgentType.ZERO_SHOT_REACT_DESCRIPTION,
     verbose = False,
     handle_parsing_errors = True
 )
@@ -72,16 +72,29 @@ if "messages" not in st.session_state:
     st.session_state['messages'] = [{
         'role': 'assistant',
         'content': 'Hello! I am an AI assistant. I can help you with math problems and data search. How can I help you today?'
-        }
-    ]
+    }]
 
 for msg in st.session_state['messages']:
     st.chat_message(msg['role']).write(msg['content'])
-    
 
 ## Function to generate a response
 def generateResponse(question):
     response = assistantAgent.invoke({'input': question})
     return response
 
- 
+## User Interaction
+userQuestion = st.text_area("Ask me a question", "What is the numerical value of pi to 10 decimal places? Please provide only the numerical result, without any code or explanations.")
+
+if st.button("Find My Answer"):
+    if userQuestion:
+        with st.spinner("Generating Answer..."):
+            st.session_state.messages.append({'role': 'user', 'content': userQuestion})
+            st.chat_message('user').write(userQuestion)
+
+            streamlitCallback = StreamlitCallbackHandler(st.container(), expand_new_thoughts=True)
+            response = assistantAgent.run(userQuestion, callbacks=[streamlitCallback])
+            st.session_state.messages.append({'role': 'assistant', 'content': response})
+            st.write('### Response: ')
+            st.success(response)
+    else:
+        st.warning("Please enter a question to get an answer.")
